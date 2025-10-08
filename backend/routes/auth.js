0843e426d77046d822b30dfcd8419534886bc5f7
Nathaniel Cedric Rabanal <86027440+ninathan@users.noreply.github.com>
2025-10-09 00:52:36 +0800

    big fixed?

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import { supabase } from "../supabaseClient.js";
+import supabase from "../supabaseClient.js";
 
 const router = express.Router();
 
