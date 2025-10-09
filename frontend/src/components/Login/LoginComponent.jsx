f1147bf0d0234e0fd5f00ebfe73d8398e888fbad
Nathaniel Cedric Rabanal <86027440+ninathan@users.noreply.github.com>
2025-10-09 13:09:46 +0800

    login fixed

--- frontend/src/components/Login/LoginComponent.jsx
+++ frontend/src/components/Login/LoginComponent.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     setLoading(true)
 
     try {
-      const res = await fetch('https://gatsis-hub.vercel.app/api/auth/login', {
+      const res = await fetch('https://gatsis-hub.vercel.app/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ emailAddress, password }),
